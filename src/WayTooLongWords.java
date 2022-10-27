4d76d76accf728f4b8ebe5e2eab33103045dfe43
John Vinh <john.michael.vinh2@gmail.com>
2022-10-27 10:27:03 -0400

    Solve WayTooLongWords

--- src/WayTooLongWords.java
+++ src/WayTooLongWords.java
@@ -1,2 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class WayTooLongWords {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+        // n test cases
+        String[] out = new String[n];
+        for (int i = 0; i < n; i++) {
+            String testCase = in.readLine();
+            out[i] = solve(testCase);
+        }
+        for (int i = 0; i < n; i++) {
+            System.out.println(out[i]);
+        }
+        in.close();
+    }
+
+    private static String solve(String testCase) {
+        if (testCase.length() <= 10) {
+            return testCase;
+        }
+        StringBuilder out = new StringBuilder();
+        char firstLetter = testCase.charAt(0);
+        char lastLetter = testCase.charAt(testCase.length() - 1);
+        out.append(firstLetter);
+        int numBetween = testCase.length() - 2;
+        out.append(numBetween);
+        out.append(lastLetter);
+        return out.toString();
+    }
 }
