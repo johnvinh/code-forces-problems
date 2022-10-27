a4613d789c815c13b08792190b57a1deb230f1c0
John Vinh <john.michael.vinh2@gmail.com>
2022-10-27 10:14:30 -0400

    Solve Watermelon

--- src/Watermelon.java
+++ src/Watermelon.java
@@ -1,2 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Watermelon {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(in.readLine());
+        boolean result = solve(number);
+        if (result) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+        in.close();
+    }
+
+    private static boolean solve(int n) {
+        for (int i = 1; i < n; i++) {
+            int number = n - i;
+            if ((number % 2 == 0) && (i % 2 == 0)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
