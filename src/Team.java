f330eddefd7ed6161bcba7cff5b2fdc77fc35498
John Vinh <john.michael.vinh2@gmail.com>
2022-10-28 10:07:13 -0400

    Solve Team

--- src/Team.java
+++ src/Team.java
@@ -6,13 +6,23 @@ public class Team {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(in.readLine());
+        int implementedProblems = 0;
         for (int i = 0; i < n; i++) {
-            
+            String line = in.readLine();
+            String[] numbers = line.split(" ");
+            int num1 = Integer.parseInt(numbers[0]);
+            int num2 = Integer.parseInt(numbers[1]);
+            int num3 = Integer.parseInt(numbers[2]);
+            boolean result = solve(num1, num2, num3);
+            if (result) {
+                implementedProblems++;
+            }
         }
+        System.out.println(implementedProblems);
         in.close();
     }
 
-    public boolean solve() {
-        return false;
+    private static boolean solve(int num1, int num2, int num3) {
+        return (num1 + num2 + num3) >= 2;
     }
 }
