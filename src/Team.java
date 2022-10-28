7a56e56f07b712eebb34df849f64444aaf07f6b9
John Vinh <john.michael.vinh2@gmail.com>
2022-10-28 09:44:41 -0400

    Implement reading n test cases

--- src/Team.java
+++ src/Team.java
@@ -5,6 +5,10 @@ import java.io.InputStreamReader;
 public class Team {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+        for (int i = 0; i < n; i++) {
+            
+        }
         in.close();
     }
 
