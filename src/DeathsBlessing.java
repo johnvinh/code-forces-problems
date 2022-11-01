98f153c993d31220569a2b2bdd9edbf25080ba11
John Vinh <john.michael.vinh2@gmail.com>
2022-11-01 12:00:36 -0400

    Add input taking

--- src/DeathsBlessing.java
+++ src/DeathsBlessing.java
@@ -1,4 +1,12 @@
 // https://codeforces.com/problemset/problem/1749/B
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class DeathsBlessing {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        in.close();
+    }
 }
