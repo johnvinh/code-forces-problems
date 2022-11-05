31b95c7085d90d7b414e0f428db95ea70768f556
John Vinh <john.michael.vinh2@gmail.com>
2022-11-05 17:25:26 -0400

    Sort input and print eventual output

--- src/Twins.java
+++ src/Twins.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Twins {
     public static void main(String[] args) throws IOException {
@@ -12,10 +13,12 @@ public class Twins {
         for (int i = 0; i < coinValueStrings.length; i++) {
             coinValues.add(Integer.parseInt(coinValueStrings[i]));
         }
+        System.out.println(solve(coinValues));
         in.close();
     }
 
     private static int solve(ArrayList<Integer> coinValues) {
+        Collections.sort(coinValues);
         return 0;
     }
 }
