2b13550f9ca912977156b6f6422c2f0840c40aa1
John Vinh <john.michael.vinh2@gmail.com>
2022-11-05 17:15:23 -0400

    Create br to prepare for taking input

--- src/Twins.java
+++ src/Twins.java
@@ -1,2 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Twins {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        in.close();
+    }
 }
