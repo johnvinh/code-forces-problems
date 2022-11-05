7d6018947dca4320de380d09356733133e7d9efa
John Vinh <john.michael.vinh2@gmail.com>
2022-11-05 17:19:48 -0400

    Complete input handling

--- src/Twins.java
+++ src/Twins.java
@@ -1,10 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Twins {
     public static void main(String[] args) throws IOException {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(in.readLine());
+        String[] coinValueStrings = in.readLine().split(" ");
+        ArrayList<Integer> coinValues = new ArrayList<>();
+        for (int i = 0; i < coinValueStrings.length; i++) {
+            coinValues.add(Integer.parseInt(coinValueStrings[i]));
+        }
         in.close();
     }
 
