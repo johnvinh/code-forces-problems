535033aee01b4fb99b70e541cae128f2a5c309b0
John Vinh <john.michael.vinh2@gmail.com>
2022-11-24 12:15:40 -0500

    Add try-with-resources for handling input

--- src/BitPlusPlus.java
+++ src/BitPlusPlus.java
@@ -1,5 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class BitPlusPlus {
     public static void main(String[] args) {
-        
+        try (InputStreamReader is = new InputStreamReader(System.in); BufferedReader in = new BufferedReader(is)) {
+            
+        } catch (IOException e) {
+
+        }
     }
 }
